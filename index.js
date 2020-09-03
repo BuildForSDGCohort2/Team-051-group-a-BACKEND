c4940b96614c285747aeeacc86b53c1911cabdb7
Kenneth Ijama <4natcom@gmail.com>
2020-09-03 06:17:07 +0100

    Fix No-Console error

--- index.js
+++ index.js
@@ -2,33 +2,31 @@
 require("dotenv").config();
 
 const app = require("./app");
-const db = require("./models/connection");
 
 // this matches all routes and all methods
 app.use((req, res, next) => {
-    res.status(404).send({
-        status: 404,
-        error: "Not found",
-    });
+  res.status(404).send({
+    status: 404,
+    error: "Not found",
+  });
 });
 
 // error handler middleware
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).send({
-        error: {
-            status: error.status || 500,
-            message: error.message || "Internal Server Error",
-        },
-    });
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || "Internal Server Error",
+    },
+  });
 });
 
 app.set("port", process.env.PORT || 8600);
-
-
+/* eslint no-console: ["error", { allow: ["debug", "error"] }] */
 const server = app.listen(app.get("port"), () => {
-    console.debug(
-        `Connected & Express is Serving on → PORT http://127.0.0.1:${
-        server.address().port
-      }`
-    );
+  console.debug(
+    `Connected & Express is Serving on → PORT http://127.0.0.1:${
+      server.address().port
+    }`
+  );
 });
